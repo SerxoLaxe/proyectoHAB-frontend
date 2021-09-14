74d0f481b3a8892a7f1857abde352dfacdcedca2
SerxoLaxe <serxolaxe@gmail.com>
2021-09-15 00:26:27 +0200

    Creada login page junto con sus componentes minimos

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import LoginPage from './pages/LoginPage';
+import LandingPage from './pages/LandingPage';
+import { UserTokenContextProvider } from './contexts/UserTokenContext';
 
 function App() {
   return (
     <Router>
-       <Switch>
-         <Route path="/"></Route>
-       </Switch>
+      <Switch>
+        <UserTokenContextProvider>
+          <Route exact path="/">
+            <LandingPage />
+          </Route>
+          <Route path='/login'>
+            <LoginPage />
+          </Route>
+        </UserTokenContextProvider>
+      </Switch>
     </Router>
   );
 }
---
+++ frontend/src/pages/LandingPage/index.js
@@ -0,0 +1,11 @@
+
+
+const LandingPage = () => {
+  return (
+    <div className='landing-page'>
+     <p>Landing page</p>
+    </div>
+  )
+}
+
+export default LandingPage;
