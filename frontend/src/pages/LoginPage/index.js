d3efd1587310e9ee75fbb2290c4a906d0c5c9191
SerxoLaxe <serxolaxe@gmail.com>
2021-09-15 16:57:08 +0200

    Añadido favicon y logotipo

--- frontend/src/pages/LoginPage/index.js
+++ frontend/src/pages/LoginPage/index.js
@@ -1,13 +1,17 @@
 import LoginForm from "../../components/LoginForm";
 import Footer from "../../components/Footer";
 import './style.css';
+import { Link } from "react-router-dom";
 
-const LoginPage = () =>{
+const LoginPage = () => {
   return (
     <div className='login-page'>
-        <img className='logo' alt='logo' src='logo192.png'/>
-        <LoginForm className='loginform'/>
-        <Footer className='footer'/>
+      <Link className='logo' to='/'>
+        <img className='logo' alt='logo' src='logo.svg' />
+        </Link>
+
+      <LoginForm className='loginform' />
+      <Footer className='footer' />
     </div>
   );
 }
