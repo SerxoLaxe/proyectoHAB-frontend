3b6e7f6430ff1801a8d55d2eb52ee12e0a4bad6b
SerxoLaxe <serxolaxe@gmail.com>
2021-09-21 01:18:51 +0200

    desarrollado componente edición perfil

--- frontend/src/components/UserProfile/index.js
+++ frontend/src/components/UserProfile/index.js
@@ -1,17 +1,20 @@
 import { useEffect, useState } from "react";
-import { useParams} from "react-router";
+import { useParams, Redirect } from "react-router";
 import { useUserTokenContext } from "../../contexts/UserTokenContext";
 import useUserProfile from '../../hooks/useUserProfile';
+import EditUserForm from '../EditUserForm';
+import './style.css';
 
 const UserProfile = () => {
   const { id } = useParams();
   const [token] = useUserTokenContext();
   const [loggedUser] = useUserProfile(token);
   const [user, setUser] = useState();
+  const [isEditable, setIsEditable] = useState(false);
+
 
   useEffect(() => {
-
-    async function fetchSearch() {
+    async function fetchUser() {
       const res = await fetch(
         `${process.env.REACT_APP_BACKEND_URL}/usuarios/${id}`,
         {
@@ -26,20 +29,39 @@ const UserProfile = () => {
       if (res.ok) {
         //setError("");
         const body = await res.json();
-        setUser(body.data);
+        setUser(body.data[0]);
       } else {
         // const error = await res.json();
         //setError(error.message);
       }
     }
-    fetchSearch();
-  }, [id, token])
-  console.log(user);
+
+    fetchUser();
+  }, [id, token]);
+
+  if (!token) {
+    return <Redirect to="/login" />;
+  }
 
   return (
-    <div>
-      <img />
-      <p>Profile {`${id}`}</p>
+    <div className='profile-wrapper'>
+
+      {isEditable
+        ?
+        (<>
+          <EditUserForm data={user} />
+          <button onClick={(e) => { setIsEditable(false) }}>Cancelar</button>
+        </>)
+        :
+        <>
+          <img className='user-avatar' src={`${process.env.REACT_APP_BACKEND_URL}/${user?.avatar || 'default-avatar.jpg'}`} alt={`avatar de ${user?.nombre}`} />
+          <div className='profile-info-div'>
+            <h1>{user?.nombre}</h1>
+            <p>{user?.biografia}</p>
+            <button type='button' onClick={(e) => { setIsEditable(true) }}>Editar perfil</button>
+          </div>
+        </>}
+
     </div>
   );
 }
