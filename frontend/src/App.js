e24ac3426eb14574a757d604c9fb3ad64e3286fb
cristina <dominguez.ac@gmail.com>
2021-09-24 20:26:02 +0200

    Pagina de experiencia

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,33 @@
-import './App.css';
+import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import LoginPage from './pages/LoginPage';
-import LandingPage from './pages/LandingPage';
-import { UserTokenContextProvider } from './contexts/UserTokenContext';
-import HomePage from './pages/HomePage';
-import { SearchResultContextProvider } from './contexts/searchResultContext';
+import LoginPage from "./pages/LoginPage";
+import LandingPage from "./pages/LandingPage";
+import { UserTokenContextProvider } from "./contexts/UserTokenContext";
+import HomePage from "./pages/HomePage";
+import { SearchResultContextProvider } from "./contexts/searchResultContext";
+import ExperiencePage from "./pages/ExperiencePage";
+import CreateExperiencePage from "./pages/CreateExperiencePage";
 
 function App() {
   return (
     <Router>
       <Switch>
         <UserTokenContextProvider>
-          <Route path='/login'>
+          <Route path="/login">
             <LoginPage />
           </Route>
           <SearchResultContextProvider>
-            <Route path='/home'>
+            <Route path="/home">
               <HomePage />
             </Route>
             <Route exact path="/">
               <LandingPage />
+            </Route>
+            <Route path="/experiencias/:id">
+              <ExperiencePage />
+            </Route>
+            <Route path="/create/experiencias">
+              <CreateExperiencePage />
             </Route>
           </SearchResultContextProvider>
         </UserTokenContextProvider>
