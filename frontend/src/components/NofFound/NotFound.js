c13b8641a736ee0333cb28d2a38024916b0eeb86
SerxoLaxe <serxolaxe@gmail.com>
2021-09-24 14:03:17 +0200

    Mejorado estilo de página error 404

--- frontend/src/components/NofFound/NotFound.js
+++ frontend/src/components/NofFound/NotFound.js
@@ -1,10 +1,14 @@
 import RollingTumbleweed from "../../animations/RollingTumbleweed";
+import './style.css'
 
-const NotFound = () =>{
+const NotFound = () => {
   return (
-    <div>
-      <h1>Error 404</h1>
-      <RollingTumbleweed/>
+    <div className='not-found'>
+      <div className='error-info-div'>
+        <h1>Error 404</h1>
+        <h2>Página no encontrada</h2>
+      </div>
+      <RollingTumbleweed className='rolling-tumbleweed' />
     </div>
   )
 }
