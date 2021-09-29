0e1def5ce52a794481ecc8db0e624d7b99182682
SerxoLaxe <serxolaxe@gmail.com>
2021-09-29 18:56:04 +0200

    Adaptado componente HomeSection para usar useProximasExperiencias

--- frontend/src/components/HomeSection/index.js
+++ frontend/src/components/HomeSection/index.js
@@ -1,7 +1,39 @@
-const HomeSection =()=>{
-  return(
+import useProximasExperiencias from "../../hooks/useProximasExperiencias";
+import List from "../List";
+import ExperienceSmall from "../ExperienceSmall";
+import './style.css'
+
+const HomeSection = () => {
+  const [experiencias, loadingExperiencias] = useProximasExperiencias();
+
+  return (
     <div>
-      <h1>HOME SECTION</h1>
+      <div className='search-result-wrapper'>
+        <h2>Próximos eventos</h2>
+        {!loadingExperiencias
+          ?
+          <>
+            {experiencias.length > 0
+              ?
+              <List
+                className='experiences-grid'
+                data={experiencias}
+                render={(experiencia) =>
+                (<ExperienceSmall className='experience' key={experiencia.id}
+                  name={experiencia.nombre}
+                  description={experiencia.descripcion}
+                  rating={experiencia.rating}
+                  price={experiencia.precio}
+                  seats={experiencia.plazas_totales}
+                  id={experiencia.id}
+                  thumbnails={experiencia.thumbnails} />)
+                } />
+              :
+              <p>No hay resultados</p>}
+          </>
+          :
+          <p>Cargango ...</p>}
+      </div>
     </div>
   )
 }
