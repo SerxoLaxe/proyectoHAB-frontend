d1da5421b6d8bcd42985e9d0f916586f9c3c1b15
SerxoLaxe <serxolaxe@gmail.com>
2021-10-05 05:39:12 +0200

    Eliminados archivos generados por error

--- frontend/src/components/ExperienceSection/index.js
+++ frontend/src/components/ExperienceSection/index.js
@@ -3,12 +3,11 @@ import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a lo
 import { Carousel } from "react-responsive-carousel";
 import { useParams, useHistory } from 'react-router-dom'
 import ReactStars from 'react-rating-stars-component';
-import Stars from '../Stars';
 import useExperiencia from "../../hooks/useExperiencia";
 import useUsersPart from "../../hooks/useUsersPart";
 import { useUserTokenContext } from "../../contexts/UserTokenContext";
 import decodeTokenData from "../../helpers/decodeTokenData";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import ParticipantsList from "../ParticipantsList";
 
 const ExperienceSection = () => {
@@ -40,20 +39,16 @@ const ExperienceSection = () => {
 
       // Controlamos si el usuario tiene reserva y si está ya se ha agotado ( experiencia ya disfrutada).
       if (participantes.length > 0) {
-        const experienceStartDate = new Date(experiencia.fecha_inicial);
+        //const experienceStartDate = new Date(experiencia.fecha_inicial);
         const experienceEndingDate = new Date(experiencia.fecha_final);
+        const currentDate = new Date();
         participantes.forEach((participante) => {
           if (participante.id_usuario === decodedToken.id) {
-            if (experienceEndingDate <= new Date()) {
+            if (experienceEndingDate <= currentDate) {
               setUserReservation('finished');
-              console.log('experiencia terminada, ahora puedes puntuarla');
               checkIfUserAlreadyRated();
-            } else if (experienceStartDate <= new Date() && experienceEndingDate >= new Date()) {
-              setReservationState('underway')
-              console.log('Experiencia en curso, ya no se puede cancelar la reserva');
             } else {
               setUserReservation('pending');
-              console.log('Experiencia pendiente de ser realizada');
             }
           }
         });
@@ -130,7 +125,7 @@ const ExperienceSection = () => {
 
   function eliminarExperiencia() {
     async function fetchEliminarExperiencia() {
-      const res = await fetch(`${process.env.REACT_APP_BACKEND_URL}/experiencias/${id}`,
+      await fetch(`${process.env.REACT_APP_BACKEND_URL}/experiencias/${id}`,
         {
           method: 'DELETE',
           headers: {
@@ -138,6 +133,7 @@ const ExperienceSection = () => {
           }
         })
     }
+
     const userInput = window.confirm('Estás seguro de que quieres eliminar esta experiencia?');
     if (userInput) {
       fetchEliminarExperiencia();
@@ -166,7 +162,7 @@ const ExperienceSection = () => {
     setRatingSelectorConfig(
       {
         edit: false,
-        count: 8,
+        count: 4,
         activeColor: 'black',
         size: 40,
         value: ratingValue,
@@ -175,15 +171,14 @@ const ExperienceSection = () => {
   }
 
   return (
-    <>
-      {!loadingExperiencia && <div className="experience-wrapper">
-        <div className='experience-info'>
-          <div className="experiencia_info_izquierda">
-            <div className="experiencia_titulo">
-              {experiencia.rating > 0 && <Stars rating={experiencia.rating} />}
-              <h2 className="experiencia_titulo">{experiencia.nombre}</h2>
-            </div>
-
+    <div className='experience-section-wrapper'>
+      {!loadingExperiencia &&
+        <>
+          <div className="experience-title">
+          <h2 className="experiencia_titulo">{experiencia.nombre}</h2>
+            {experiencia.rating > 0 && <ReactStars edit={false} value={experiencia.rating} activeColor='black' color='transparent' count={4} size={30} />}
+          </div>
+          <div className="experience-carrousel">
             {experiencia.fotos.length > 0 ? (
               <Carousel
                 infiniteLoop={true}
@@ -194,6 +189,7 @@ const ExperienceSection = () => {
               >
                 {experiencia.fotos.map((foto) => (
                   <img
+                  className='experience-image'
                     src={`${process.env.REACT_APP_BACKEND_URL}/fotos/${foto.foto}`}
                     key={foto.id}
                     alt="Foto de la experiencia"
@@ -204,59 +200,56 @@ const ExperienceSection = () => {
               <p>No hay fotos</p>
             )}
           </div>
+          <div className='experience-info'>
+              <p className="experiencia_fechas">{`del ${new Date(
+                experiencia.fecha_inicial
+              ).toLocaleDateString()} al ${new Date(
+                experiencia.fecha_final
+              ).toLocaleDateString()} en ${experiencia.ubicacion}`}</p>
 
-          <div className="experiencia_info_derecha">
-            <p className="experiencia_fechas">{`del ${new Date(
-              experiencia.fecha_inicial
-            ).toLocaleDateString()} al ${new Date(
-              experiencia.fecha_final
-            ).toLocaleDateString()} en ${experiencia.ubicacion}`}</p>
+              <div className="experiencia_plazas">
+                <p className="experiencia_plazas_disponibles">{`${experiencia.plazas_totales - participantes.length} plazas libres`}</p>
+                <p className="experiencia_plazas_totales">{`de ${experiencia.plazas_totales} plazas en total`}</p>
+              </div>
+              <p className="experiencia_descripcion">{experiencia.descripcion}</p>
+              <div className="experiencia_precio">
+                <h2>por {`${experiencia.precio} €`}</h2>
+              </div>
 
-            <div className="experiencia_plazas">
-              <p className="experiencia_plazas_disponibles">{`${experiencia.plazas_totales - participantes.length} plazas cubiertas`}</p>
-              <p className="experiencia_plazas_totales">{`de ${experiencia.plazas_totales} plazas en total`}</p>
-            </div>
-            <p className="experiencia_descripcion">{experiencia.descripcion}</p>
-            <div className="experiencia_precio">
-              <h2>por {`${experiencia.precio} €`}</h2>
-            </div>
-
-            {loggedUser
-              ?
-              <>
-                {loggedUser.role === 'admin'
-                  &&
-                  <>
-                    <button>Editar</button>
-                    <button onClick={eliminarExperiencia}>Eliminar</button>
-                  </>}
-                {userReservation === 'pending' && <button onClick={cancelarReserva}>Cancelar reserva</button>}
-                {userReservation === '' && <button onClick={reservarExperiencia}>Reservar</button>}
-                {userReservation === 'finished' &&
-                  <>
-                    <ReactStars {...ratingSelectorConfig} />
-                    {userRated && <p>Ya has puntuado esta experiencia</p>}
-                  </>
-                }
-              </>
-              :
-              <p>Inicia sesión para poder realizar tu reserva.</p>
-            }
+              {loggedUser
+                ?
+                <>
+                  {loggedUser.role === 'admin'
+                    &&
+                    <>
+                      <button>Editar</button>
+                      <button onClick={eliminarExperiencia}>Eliminar</button>
+                    </>}
+                  {userReservation === 'pending' && <button onClick={cancelarReserva}>Cancelar reserva</button>}
+                  {userReservation === '' && <button onClick={reservarExperiencia}>Reservar</button>}
+                  {userReservation === 'finished' &&
+                    <>
+                      <ReactStars {...ratingSelectorConfig} />
+                      {userRated && <p>Ya has puntuado esta experiencia</p>}
+                    </>
+                  }
+                </>
+                :
+                <p>Inicia sesión para poder realizar tu reserva.</p>
+              }
           </div>
-        </div>
-      </div>}
-
-      {loggedUser && !loadingParticipantes &&
-        <>
-          {participantes.length > 0
-            ?
-            <ParticipantsList participants={participantes} loggedUserId={loggedUser.id} userReservationState={userReservation} />
-            :
-            <p>Todavía no hay nadie apuntado</p>
+          {loggedUser && !loadingParticipantes &&
+            <div className='experience-participants'>
+              {participantes.length > 0
+                ?
+                <ParticipantsList participants={participantes} loggedUserId={loggedUser.id} userReservationState={userReservation} />
+                :
+                <p>Todavía no hay nadie apuntado</p>
+              }
+            </div>
           }
-        </>
-      }
-    </>
+        </>}
+    </div>
   );
 };
 
